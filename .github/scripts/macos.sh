634c85fc99a9513d88fd1c2619cd3431993f4aa9
Stéphane Derosiaux <derste@gmail.com>
2020-03-21 21:54:32 +0100

    Add keychain stuff

--- .github/scripts/macos.sh
+++ .github/scripts/macos.sh
@@ -30,6 +30,27 @@ jlink --module-path "$FX_MODS_PATH" \
     --strip-debug --compress 2 --no-header-files --no-man-pages --strip-native-commands
 CUSTOM_JRE_PATH="$(pwd)/$CUSTOM_JRE_NAME"
 
+###############################################################################
+# Configure Keychain for signing
+###############################################################################
+if  [ "$IDENTITY_PASSPHRASE" != "" ] && [ "$IDENTITY_P12_B64" != "" ]; then
+  security create-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
+  security default-keychain -s build.keychain
+  security unlock-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
+
+  # Put the base-64 encoded signing certificicate into a text file, decode it to binary form.
+  echo "$IDENTITY_P12_B64" > DS_ID_App.p12.txt
+  openssl base64 -d -in DS_ID_App.p12.txt -out DS_ID_App.p12
+
+  # Install the decoded signing certificate into our unlocked build keychain.
+  security import DS_ID_App.p12 -A -P "$IDENTITY_PASSPHRASE"
+
+  # Set the keychain to allow use of the certificate without user interaction (we are headless!)
+  security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
+fi
+###############################################################################
+
+
 # needs the original source here
 DEPLOY_RESOURCES_PATH=".github/resources"
 echo "Packaging .pkg"
@@ -49,7 +70,6 @@ jpackage --name "$CDK_APP_NAME" \
               --input "$CONDUKTOR_DISTRIBUTION_PATH/lib" \
               --main-jar "desktop-$VERSION.jar" \
               --runtime-image "$CUSTOM_JRE_PATH"
-# --mac-sign --mac-signing-key-user-name "developer"  --mac-package-signing-prefix com.myapp
 
 DMG=false
 if $DMG; then
