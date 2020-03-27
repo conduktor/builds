d5b8aa44e43bb98691919ad9be9c9c2f03dad54a
Stéphane Derosiaux <derste@gmail.com>
2020-03-27 11:21:18 +0100

    Filter non-desired changelog commits

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -17,7 +17,7 @@ NAME="$VERSION ($(date -u "+%Y-%m-%d"))"
 
 CURRENT_TAG=$(git describe --abbrev=0 --tags)
 PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)
-CHANGELOG=$(git log --no-merges --pretty=%s "$PREVIOUS_TAG".."$CURRENT_TAG" | grep -v "skip ci" | sed -e 's/^/- /')
+CHANGELOG=$(git log --no-merges --pretty=%s "$PREVIOUS_TAG".."$CURRENT_TAG" | grep -v "skip ci" | grep -v "skip changelog" | sed -e 's/^/- /')
 
 # These changes WILL appear in Conduktor Desktop UI
 # This should contain only text context-independent (no "Download links below" for instance)
