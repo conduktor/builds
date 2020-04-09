f67d44e29d2bd34910800665def809b676fec46a
Stéphane Derosiaux <derste@gmail.com>
2020-04-09 17:21:00 +0200

    Update website with changelogs

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -20,8 +20,10 @@ CURRENT_TAG=$(git describe --abbrev=0 --tags)
 PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git describe --abbrev=0)^)
 CHANGELOG=$(git log --no-merges --pretty=%s "$PREVIOUS_TAG".."$CURRENT_TAG" | grep -v "skip ci" | grep -v "skip changelog" | sed -e 's/^/- /')
 
+#
 # These changes WILL appear in Conduktor Desktop UI
 # This should contain only text context-independent (no "Download links below" for instance)
+#
 BODY=$(cat <<-EOF
 ### Conduktor Desktop v${VERSION} ($DATE)
 
@@ -33,5 +35,22 @@ BODY="${BODY//'%'/'%25'}"
 BODY="${BODY//$'\n'/'%0A'}"
 BODY="${BODY//$'\r'/'%0D'}"
 
+
+#
+# YAML body for the website
+#
+BODY_MD=$(cat <<-EOF
+---
+date: $DATE
+--
+$CHANGELOG
+EOF
+)
+# See https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/m-p/38372#M3322
+BODY_MD="${BODY_MD//'%'/'%25'}"
+BODY_MD="${BODY_MD//$'\n'/'%0A'}"
+BODY_MD="${BODY_MD//$'\r'/'%0D'}"
+
 echo "::set-output name=name::$NAME"
 echo "::set-output name=body::$BODY"
+echo "::set-output name=bodymd::$BODY_MD"
