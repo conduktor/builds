04e51bbb1e737c0e78610f393f326bcb29fc59a3
Stéphane Derosiaux <derste@gmail.com>
2020-04-13 03:02:07 +0200

    Add pkg signing

--- .github/scripts/macos.sh
+++ .github/scripts/macos.sh
@@ -8,26 +8,50 @@ FX="https://gluonhq.com/download/javafx-14-jmods-mac"
 CURRENT_DIR=$(pwd)
 echo "Current dir: $CURRENT_DIR"
 
-VERSION="$1"
+VERSION="${1:?Missing version (1.2.3)}"
 echo "Will build Conduktor $VERSION"
+
+echo "Modules: ${CDK_JLINK_MODULES:?Missing modules}"
+echo "Application: ${CDK_APP_NAME:?Missing application name}"
+echo "Description: ${CDK_APP_DESCRIPTION:?Missing description}"
+echo "Vendor: ${CDK_VENDOR:?Missing vendor}"
+echo "Copyright: ${CDK_COPYRIGHT:?Missing copyright}"
+echo "Options: ${CDK_JAVA_OPTIONS:?Missing Java Options}"
+
+########################################################################################################################
+
+CONDUKTOR_DISTRIBUTION_PATH="$(pwd)/conduktor-$VERSION"
+DEPLOY_RESOURCES_PATH=".github/resources"
+APP="$CDK_APP_NAME.app"
+
+if [ ! -d "$CONDUKTOR_DISTRIBUTION_PATH" ]; then
+  echo "Ensure $CONDUKTOR_DISTRIBUTION_PATH exists"
+  exit 1
+fi
+if [ ! -d "$DEPLOY_RESOURCES_PATH" ]; then
+  echo "Ensure $DEPLOY_RESOURCES_PATH exists"
+  exit 1
+fi
 
 ########################################################################################################################
 
 echo "Downloading JavaFX Jmods..."
 curl -sLO $FX
 unzip -oq javafx-*-jmods-mac
+# cleanup
+rm -f javafx-14-jmods-mac
 FX_MODS_PATH="./javafx-jmods-14"
-
-CONDUKTOR_DISTRIBUTION_PATH="$(pwd)/conduktor-$VERSION"
 
 echo "Building custom JRE..."
 CUSTOM_JRE_NAME="runtime"
+
+rm -rf "$CUSTOM_JRE_NAME"
 jlink --module-path "$FX_MODS_PATH" \
     --add-modules "$CDK_JLINK_MODULES" \
     --bind-services --output "$CUSTOM_JRE_NAME" \
     --strip-debug --compress 2 --no-header-files --no-man-pages --strip-native-commands
-
-DEPLOY_RESOURCES_PATH=".github/resources"
+# cleanup
+rm -rf "$FX_MODS_PATH"
 
 ###############################################################################
 # Configure Keychain for signing
@@ -37,23 +61,27 @@ xcrun --version
 xcrun --show-sdk-version
 xcrun --show-sdk-build-version
 
-IDENTITY_PASSPHRASE=${IDENTITY_PASSPHRASE:-""}
-if  [ "$IDENTITY_PASSPHRASE" != "" ] && [ "$IDENTITY_P12_B64" != "" ]; then
-  security create-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
+if [ "$MACOS_SIGNING_IDENTITY_PASSPHRASE" != "" ] && [ "$MACOS_SIGNING_IDENTITY_B64" != "" ]; then
+  if [ "$MACOS_SIGNING_USERNAME" = "" ] || [ "$MACOS_SIGNING_SPECIFIC_PWD" = "" ]; then
+    echo "Missing auth to notarization service (MACOS_SIGNING_*) aborting..."
+    exit 1
+  fi
+
+  #security create-keychain -p "$MACOS_SIGNING_IDENTITY_PASSPHRASE" build.keychain
   security default-keychain -s build.keychain
-  security unlock-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
-
-  # Put the base-64 encoded signing certificicate into a text file, decode it to binary form.
-  echo "$IDENTITY_P12_B64" > DS_ID_App.p12.txt
-  openssl base64 -d -in DS_ID_App.p12.txt -out DS_ID_App.p12
-
-  # Install the decoded signing certificate into our unlocked build keychain.
-  security import DS_ID_App.p12 -A -P "$IDENTITY_PASSPHRASE"
-
+  security unlock-keychain -p "$MACOS_SIGNING_IDENTITY_PASSPHRASE" build.keychain
+	security set-keychain-settings -t 3600 -u build.keychain
+  echo "$MACOS_SIGNING_IDENTITY_B64" > Conduktor.p12.txt
+  openssl base64 -d -in Conduktor.p12.txt -out Conduktor.p12
+  security import ./Conduktor.p12 -k build.keychain -T /usr/bin/codesign -T /usr/bin/security -T /usr/bin/productsign -P "$MACOS_SIGNING_IDENTITY_PASSPHRASE"
+  rm -f Conduktor.p12.txt Conduktor.p12
   # Set the keychain to allow use of the certificate without user interaction (we are headless!)
-  security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
+  security set-key-partition-list -S apple-tool:,apple: -s -k "$MACOS_SIGNING_IDENTITY_PASSPHRASE" build.keychain
+  # check it's there
+  security find-identity -v -p codesigning build.keychain
 
-  # build raw-image
+  # build raw image
+  rm -rf "$APP"
   jpackage --name "$CDK_APP_NAME" \
               --app-version "$VERSION" \
               --description "$CDK_APP_DESCRIPTION" \
@@ -69,29 +97,55 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ] && [ "$IDENTITY_P12_B64" != "" ]; then
               --dest . \
               --input "$CONDUKTOR_DISTRIBUTION_PATH/lib" \
               --main-jar "desktop-$VERSION.jar" \
-              --runtime-image "$CUSTOM_JRE_NAME"
+              --runtime-image "$CUSTOM_JRE_NAME" \
+              --java-options "$CDK_JAVA_OPTIONS"
+  # cleanup
+  rm -rf "$CUSTOM_JRE_NAME"
 
   # let's sign it
-  APP="$CDK_APP_NAME.app"
-  IDENTITY="Developer ID Application: Conduktor"
+  IDENTITY="Developer ID Application: Conduktor LLC (572B6PF39A)"
+  IDENTITY_INSTALLER="Developer ID Installer: Conduktor LLC (572B6PF39A)"
   ENTITLEMENTS_PATH="$DEPLOY_RESOURCES_PATH/Conduktor.entitlements"
 
-  echo "Signing all the content..."
-  codesign --force --strict --timestamp --verbose=4 --prefix "io.conduktor." \
-           --deep \
-           --entitlements "$ENTITLEMENTS_PATH" \
-           --options runtime \
-           --sign "$IDENTITY"
-           "$APP"
+  echo "Repackaging JavaFX..."
+  CURRENT=$(pwd)
+  for javafx in $(find $APP/Contents/app -name "javafx*-mac.jar"); do
+    GOTO=$(mktemp -d)
+    cd "$GOTO"
+    jar xf "$CURRENT/$javafx" >/dev/null
+    for dylib in $(find . -name "*.dylib"); do
+      echo "Signing $dylib..."
+      codesign --strict --keychain build.keychain --force --timestamp --verbose=4 --prefix "io.conduktor." \
+                --options runtime --sign "$IDENTITY" "$dylib"
+      echo "Repackaging $dylib into $javafx..."
+      jar uf "$CURRENT/$javafx" "$dylib"
+    done
+  done
+  cd "$CURRENT"
 
-  codesign -dvvvv "$APP"
+  echo "Signing our jars..."
+  for jar in $(find $APP/Contents/app -name "*.jar"); do
+    codesign --strict --keychain build.keychain --timestamp --verbose=4 --prefix "io.conduktor." \
+              --options runtime --force --entitlements "$ENTITLEMENTS_PATH" --sign "$IDENTITY" \
+              "$jar"
+  done
 
-  APP_SIGNED_NAME="$APP_NAME-signed"
+  echo "Signing our runtime..."
+  for runtime in $(find $APP/Contents/runtime/Contents/Home/lib -name "*.dylib") $APP/Contents/runtime/; do
+    codesign --strict --keychain build.keychain --force --timestamp --verbose=4 --prefix "io.conduktor." \
+              --deep --options runtime --sign "$IDENTITY" "$runtime"
+  done
+
+  echo "Signing the app itself"
+  codesign --strict --keychain build.keychain --force --timestamp --verbose=4 --prefix "io.conduktor." \
+           --deep --entitlements "$ENTITLEMENTS_PATH" --options runtime --sign "$IDENTITY" "$APP"
+
   echo "Finishing packaging with signed content..."
-  jpackage --name "$APP_SIGNED_NAME" \
+  jpackage --name "$CDK_APP_NAME" \
               --app-version "$VERSION" \
               --description "$CDK_APP_DESCRIPTION" \
-              --type dmg \
+              --type pkg \
+              --app-image "$APP" \
               --vendor "$CDK_VENDOR" \
               --mac-package-identifier "io.conduktor.app.Conduktor" \
               --mac-package-name "$CDK_APP_NAME" \
@@ -100,57 +154,50 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ] && [ "$IDENTITY_P12_B64" != "" ]; then
               --verbose \
               --dest .
 
+  # cleanup
+  rm -rf "$APP"
+
   echo "Signing the package..."
-  DMG_NAME="$APP_SIGNED_NAME-$VERSION.dmg"
-  codesign --strict --timestamp --verbose=4 --prefix "io.conduktor." \
-            --entitlements "$ENTITLEMENTS_PATH" \
-            --options runtime \
-            --sign "$IDENTITY" \
-            "$DMG_NAME"
+  PKG="${CDK_APP_NAME}-${VERSION}.pkg"
+  productsign --keychain build.keychain --sign "$IDENTITY_INSTALLER" "$PKG" "${CDK_APP_NAME}-${VERSION}-signed.pkg"
+  mv "${CDK_APP_NAME}-${VERSION}-signed.pkg" "$PKG"
 
-  NOTORIZATION=false
+  # cleanup
+  security delete-keychain build.keychain
 
-  if $NOTORIZATION; then
+  REQUEST_ID=$(xcrun altool \
+    --notarize-app \
+    --primary-bundle-id "io.conduktor" \
+    --username "$MACOS_SIGNING_USERNAME" --password "$MACOS_SIGNING_SPECIFIC_PWD" \
+    --file "$PKG" \
+    --output-format json | jq -r '."notarization-upload".RequestUUID')
 
-    USER="user"
-    PASSWD="pwd"
-
-    xcrun altool \
-      --notarize-app \
-      --primary-bundle-id "io.conduktor" \
-      --username "$USER" --password "$PASSWD" \
-      --file "$DMG_NAME" \
-      --output-format xml > upload_result.plist
-
-      request_id=$(/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" upload_result.plist)
-
-      # Wait until the request is done processing.
-      while true; do
-          sleep 20
-          xcrun altool --notarization-info "$request_id" \
-                --username "$USER" --password "$PASSWD" \
-                --output-format xml > status.plist
-          if [ "$(/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" status.plist)" != "in progress" ]; then
-              break;
-          fi
-          echo "$(date) ...still waiting for notarization to finish..."
-      done
-
-      # See if notarization succeeded, and if so, staple the ticket to the disk image.
-      if [ "$(/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" status.plist)" = "success" ]; then
-          echo "Notarization succeeded, stapling receipt to disk image"
-          xcrun stapler staple "$DMG_NAME"
-      else
-          false;
+  # Wait until the request is done processing.
+  STATUS=""
+  while true; do
+      sleep 20
+      STATUS=$(xcrun altool --notarization-info "$REQUEST_ID" \
+            --username "$MACOS_SIGNING_USERNAME" --password "$MACOS_SIGNING_SPECIFIC_PWD" \
+            --output-format json | jq -r '."notarization-info".Status')
+      if [ "$STATUS" != "in progress" ]; then
+          break;
       fi
+      echo "$(date) ...still waiting for notarization to finish..."
+  done
+
+  # See if notarization succeeded, and if so, staple the ticket to the disk image.
+  if [ "$STATUS" = "success" ]; then
+      echo "Notarization succeeded, stapling receipt to disk image"
+      xcrun stapler staple "$PKG"
+  else
+      echo "Notarization failed, aborting the build..."
+      exit 1
   fi
 
-fi
-###############################################################################
+else
 
-
-echo "Packaging .pkg"
-jpackage --name "$CDK_APP_NAME" \
+  echo "Packaging unsigned .pkg"
+  jpackage --name "$CDK_APP_NAME" \
               --app-version "$VERSION" \
               --description "$CDK_APP_DESCRIPTION" \
               --type pkg \
@@ -167,11 +214,11 @@ jpackage --name "$CDK_APP_NAME" \
               --main-jar "desktop-$VERSION.jar" \
               --runtime-image "$CUSTOM_JRE_NAME" \
               --java-options "$CDK_JAVA_OPTIONS"
+fi
 
 DMG=true
 if $DMG; then
     echo "Packaging .dmg"
-    echo "The .dmg sucks because: https://github.com/andreyvit/create-dmg/issues/72"
     jpackage --name "$CDK_APP_NAME" \
               --app-version "$VERSION" \
               --description "$CDK_APP_DESCRIPTION" \
