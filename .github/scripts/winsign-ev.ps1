861db3d1f32a529d2f226eff5d189347a280825b
Stephane Derosiaux <sderosiaux@conduktor.io>
2022-06-25 16:10:13 +0200

    Select the right cert

--- .github/scripts/winsign-ev.ps1
+++ .github/scripts/winsign-ev.ps1
@@ -9,17 +9,18 @@ $ErrorActionPreference = "Stop"
 $SignTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
 Try
 {
-  Write-Host "Signing .exe"
+  cd windows
 
-  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 /a "Conduktor-$Version.exe"
+  Write-Host "Signing .exe"
+  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 /n "Conduktor, Inc" "Conduktor-$Version.exe"
   Start-Sleep -s 15
 
   Write-Host "Signing .msi"
-  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 "Conduktor-$Version.msi"
+  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 /n "Conduktor, Inc" "Conduktor-$Version.msi"
   Start-Sleep -s 15
   
   Write-Host "Signing .msi Single User"
-  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 "Conduktor-$Version-single-user.msi"
+  & $SignTool sign /v /d "Conduktor Desktop v$Version" /du "https://www.conduktor.io" /tr "http://timestamp.digicert.com" /fd sha256 /td sha256 /n "Conduktor, Inc" "Conduktor-$Version-single-user.msi"
 }
 Catch {
   Write-Host "An error occurred:"
